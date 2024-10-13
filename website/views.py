5bf6596999d9229f923879f4c0377ade8eb46ce8
Nora <Nora@DESKTOP-HEH6IOF>
2024-10-13 14:22:38 +0300

    + available slots endpoint&view

--- website/views.py
+++ website/views.py
@@ -1,13 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, render_template
+import json
 
 views = Blueprint('views', __name__)
+
+
+def load_slots():
+    with open('website/static/slots.json', 'r') as f:
+        slots = json.load(f)
+    return slots
+
 
 
 @views.route("/")
 def home():
     return "here is the home page"
 
-@views.route("/available-slots")
+@views.route("/available-slots", methods=['GET'])
 def available_slots():
-    return "here is the available slots"
+    slots = load_slots()
+    return render_template("slots.html", slots=slots)
+
+@views.route('/slot-details/<int:slot_id>')
+def slot_details(slot_id):
+    # your logic here
+    return "here is the slot details"
+
 
